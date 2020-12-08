54b2f79fa6dc7d14d721860a13e35a254496edfb
zaquiel.rodriguez <zaquiel.rodriguez@datiobbva.com>
2020-12-08 07:48:48 +0100

    end basic project without server or navigation

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import {View} from 'react-native';
-import AppText from './app/components/AppText/AppText';
-import AppButton from './app/components/AppButton';
+import React from "react";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-    return <WelcomeScreen />;
-  /*return (
-        <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-            <MaterialCommunityIcons name="email" size={60} color="dodgerblue" />
-            <AppText> Example of a text</AppText>
-            <AppButton title="Login" onPress={()=>{console.log("Login tapped");}}/>
-        </View>
-  );*/
-  // return <ViewImageScreen />
-  //return <WelcomeScreen />;
+  return <ListingEditScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,52 +1,51 @@
-import React from 'react';
-import colors from '../config/colors';
-import {ImageBackground, StyleSheet,Text, Image, View, SafeAreaView} from 'react-native';
-import AppButton from '../components/AppButton';
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+import Button from "../components/Button";
 
 function WelcomeScreen(props) {
-    return (
-        <ImageBackground
-            blurRadius={10}
-            style={[styles.background]}
-            source={require('../assets/background.jpg')}>
-            <View style={styles.logoContainer}>
-                <Image source={require('../assets/logo-red.png')} style={styles.logo}/>
-                <Text style={styles.tagLine}>Sell What You Don't Need</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-                <AppButton title="Login" onPress={()=>{console.log("Login tapped");}}/>
-                <AppButton title="Register" color="secondary" onPress={()=>{console.log("Register tapped");}}/>
-            </View>
-            <View style={styles.registerButton}></View>
-        </ImageBackground>
-    );
+  return (
+    <ImageBackground
+      blurRadius={10}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button title="Login" />
+        <Button title="Register" color="secondary" />
+      </View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: 'center'
-    },
-    logoContainer: {
-        position: 'absolute',
-        top: 70,
-        alignItems: 'center'
-    },
-    logo:{
-        width: 100,
-        height: 100,
-
-    },
-    tagLine: {
-        fontSize: 25,
-        fontWeight: '600',
-        paddingVertical: 20
-    },
-    buttonContainer: {
-        padding: 20,
-        width: '100%'
-    },
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
+  },
 });
 
 export default WelcomeScreen;
